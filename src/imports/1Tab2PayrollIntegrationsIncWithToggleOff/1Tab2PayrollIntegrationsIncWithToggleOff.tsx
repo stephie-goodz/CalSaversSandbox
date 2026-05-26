63c4c82f165e1d80c065e8506a0c43865a584f11
stephie-goodz <98040245+stephie-goodz@users.noreply.github.com>
2026-05-26 10:28:03 -0400

    Add files via upload

--- src/imports/1Tab2PayrollIntegrationsIncWithToggleOff/1Tab2PayrollIntegrationsIncWithToggleOff.tsx
+++ src/imports/1Tab2PayrollIntegrationsIncWithToggleOff/1Tab2PayrollIntegrationsIncWithToggleOff.tsx
@@ -460,7 +460,7 @@ function NumberContainer() {
 function TextContainer() {
   return (
     <div className="content-stretch flex flex-[1_0_0] flex-col gap-[4px] items-start min-w-px relative" data-name="Text Container">
-      <p className="font-['Poppins',sans-serif] font-semibold leading-[24px] min-w-full not-italic relative shrink-0 text-[#00594f] text-[16px] tracking-[1px] w-[min-content]">Contact your payroll provider to set up a deduction line</p>
+      <p className="font-['Poppins',sans-serif] font-semibold leading-[24px] min-w-full not-italic relative shrink-0 text-[#00594f] text-[16px] tracking-[1px] w-[min-content]">Set up deduction line with provider</p>
     </div>
   );
 }
